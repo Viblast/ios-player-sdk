43c595c11e4e79cb8afa490a5c7d7653849bbe83
Ivan Ermolaev <ivermolaev@setelis.com>
2016-03-10 15:25:51 +0200

    Added seek with tolerance (an API for precise seek) + various bug fixes

--- VBPlayerDemo/VBPlayerDemo/VBPlayer.h
+++ VBPlayerDemo/VBPlayerDemo/VBPlayer.h
@@ -2,7 +2,6 @@
 //  VBPlayer.h
 //  VBPlayer
 //
-//  Created by Ivan Ermolaev on 4/21/15.
 //  Copyright (c) 2015 Viblast. All rights reserved.
 //
 
@@ -18,23 +17,6 @@
 #define VIBLAST_EXPORT extern
 #endif
 
-typedef NS_ENUM(NSInteger, VBPlayerStatus) {
-  VBPlayerStatusUnknown,
-  VBPlayerStatusReadyToPlay,
-  VBPlayerStatusFailed
-};
-
-VIBLAST_EXPORT NSString *const VBErrorDomain;
-
-/* 
- VBPlayer settings dictionary keys
- 
- */
-
-// Enables p2p media delivery
-VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
-
-
 /*
  VBPlayerDelegate
  
@@ -49,14 +31,19 @@ VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
 - (void)playerDidEnterStall:(VBPlayer *)player;
 - (void)playerDidExitStall:(VBPlayer *)player;
 
-- (void)playerDidFinish:(VBPlayer *)player;
+- (void)playerDidFinishPlaying:(VBPlayer *)player;
 @end
-
 
 /*
  VBPlayer
  
  */
+typedef NS_ENUM(NSInteger, VBPlayerStatus) {
+  VBPlayerStatusUnknown,
+  VBPlayerStatusReadyToPlay,
+  VBPlayerStatusFailed
+};
+
 @interface VBPlayer : NSObject
 
 // Contains the error when player's status is set to VBPlayerStatusFailed.
@@ -79,6 +66,8 @@ VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
 @property (nonatomic, weak) id<VBPlayerDelegate> delegate;
 
 - (instancetype)initWithCDN:(NSString *)cdn;
+
+// For the available settings check the constants below.
 - (instancetype)initWithCDN:(NSString *)cdn settings:(NSDictionary *)settings;
 
 - (instancetype)initWithArgs:(NSString *)args;
@@ -86,11 +75,21 @@ VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
 - (void)play;
 - (void)pause;
 
+// Seek to a given |time| (imprecise).
+// Depending on the stream the actual seeked time might not be the same as the requested one
+// for efficiency reasons. In order to perform a precised seek see seekToTime:tolerance:.
 - (void)seekToTime:(CMTime)time;
 
-// Use to receive a callback when seek is completed. Only the completion of the last seek will be
-// called, i.e. if there is a seek in progress its completion will be ignored.
+// Seek to a given |time| and provide a |tolerance| back in time to restrict the result time that is
+// the time seeked to will be in [time - tolerance, time] range.
+// Tolerance kCMTimePositiveInfinity will give the same result as seekToTime:.
+// Tolerance kCMTimeZero will perform a precise seek.
+- (void)seekToTime:(CMTime)time tolerance:(CMTime)tolerance;
+
+// Use a |completion| handler if you want to be notified when the seek operation completes.
 - (void)seekToTime:(CMTime)time completion:(void(^)(void))completion;
+- (void)seekToTime:(CMTime)time tolerance:(CMTime)tolerance completion:(void(^)(void))completion;
+
 
 // Observe periodic time changes.
 // Must retain the result in order the |block| to be called.
@@ -109,6 +108,7 @@ VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
 @end
 
 
+
 /*
  VBDataPlayer
  
@@ -117,33 +117,44 @@ VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
 
  Things to keep in mind:
  * You can manipulate the current time using -seekToTime:. By default it is set to 0.
- * The player DOESN'T CACHE any data back in time, so:
-   1) whenever you seek, all the data prior to the seeked moment will be discarded. 
-   2) it's the client's responsibility to cache or acquire the media data again and feed it when 
-   appropriate.
+ * The client should append only continuous data.
+ * Calling -seekToTime:. will discard any buffered data so far so it is a client's responsibility
+   to cache or acquire the media data again and feed it when appropriate.
  * Calling |duration| on this player will result in kCMTimeIndefinite.
- * You should again observer the player's |status| to know when the player is ready to play.
+ * You should again (as in VBPlayer) observer the |status| to know when the player is ready to play.
  
  */
 
+
+/*
+ VBDataCoding
+ Describes the possible codings of the data you wish to append to the player.
+ 
+ * VBDataCodingMP4 - Use for MP4 data with multiplexed content, e.g. AV initialization (+ AV data),
+ * VBDataCodingMP4Audio - Use for MP4 audio only data, e.g. Audio initialization (+ Audio data)
+ * VBDataCodingMP4Video - Use for MP4 video only data, e.g. Video initialization (+ Video data)
+ 
+ */
 typedef NS_ENUM(NSInteger, VBDataCoding) {
   VBDataCodingMP4,
   VBDataCodingMP4Audio,
   VBDataCodingMP4Video,
-  VBDataCodingMP4Metadata,
 };
+
 
 @interface VBDataPlayer : VBPlayer
 
+// Designated initializer
 + (instancetype)createDataPlayer;
 
-// Enqueues media |data| to be played at some point ahead of time.
+// Enqueues media |data| with certain |coding| to be played at some point ahead of time.
 // NOTE: Each |data| MUST be the continuation of the data previously appended! This does NOT hold
 // if the append is made after a seek or if this is the first append.
 - (BOOL)appendData:(NSData *)data coding:(VBDataCoding)coding error:(NSError **)outError;
 
-// Returns the buffered media time ahead. This is the minimum between the audio and the video.
-- (CMTime)bufferredDuration;
+// Returns the duration of the data segments haven't been requested for decoding yet.
+// This is the minimum between the audio and the video duration.
+- (CMTime)bufferedDuration;
 
 // Tells the player that there is no more data to be played and that it should finish when it has
 // played its last piece of data.
@@ -151,3 +162,14 @@ typedef NS_ENUM(NSInteger, VBDataCoding) {
 // again when appropriate.
 - (void)endOfStream;
 @end
+
+// Constants:
+VIBLAST_EXPORT NSString *const VBErrorDomain;
+
+// VBPlayer initialization settings:
+VIBLAST_EXPORT NSString *const VBEnablePDNKey; // NSNumber (BOOL)
+
+// VBPlayerDelegate's methods as notifications:
+VIBLAST_EXPORT NSString *const VBPlayerDidEnterStallNotification;
+VIBLAST_EXPORT NSString *const VBPlayerDidExitStallNotification;
+VIBLAST_EXPORT NSString *const VBPlayerDidFinishPlayingNotification;
